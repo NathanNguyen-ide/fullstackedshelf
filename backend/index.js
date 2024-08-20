193d63359e869e29961942b177beaf8695ed6c30
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-08-19 23:02:18 -0700

    first http route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { application } from "express";
 import {PORT} from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Bookstore')
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
