2dc5f7a5f511210a3f9f8fa96b6871c036423730
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-11 15:34:03 -0700

    adding get for all books and one book

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,35 @@ app.post('/books', async (request, response) => {
     }
 });
 
+// route to get all books from database
+app.get('/books',async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+// route to get one book from database by id
+app.get('/books/:id',async (request, response) => {
+    try {
+
+        const {id} = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
