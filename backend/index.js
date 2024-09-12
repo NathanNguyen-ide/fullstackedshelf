8767898bf26a58c831137a4990bf2b1e2788f0ab
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-11 17:15:35 -0700

    finished server

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,28 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 import {Book} from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // parsing request body, handles json data sent in http requests
 app.use(express.json());
 
+// middleware for handling CORS
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST','PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Bookstore')
 })
 
-// define middleware routing to /books
+// define middleware routing to books
 app.use('/books', booksRoute);
 
 mongoose
