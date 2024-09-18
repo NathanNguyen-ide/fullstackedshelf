b2a80518bd4aecc3aeefcd3188369f274dc862f7
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-18 15:00:16 -0700

    show in react

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,15 @@ const app = express();
 app.use(express.json());
 
 // middleware for handling CORS
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST','PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+app.use(cors());
+
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST','PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request)
