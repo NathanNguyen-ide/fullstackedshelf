bd06575bd6bc798d6f1183ed830687e205679b7b
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-23 20:05:14 -0700

    ui changes

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,22 +3,26 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import {useSnackbar} from 'notistack';
 
 const DeleteBook = () => {
   const [loading,setLoading] = useState(false);
   const navigate = useNavigate();
   const {id} = useParams();
+  const {enqueueSnackbar} = useSnackbar();
   const handleDeleteBook = () => {
     setLoading(true);
     axios
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Deleted Successfully', {variant: "success"});
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
         alert('An error happened. Please check console')
+        enqueueSnackbar('Error', {variant: "error"});
         console.log(error);
       });
   };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
         </button>
       </div>
       <div className = 'flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
+        <h1 className='text-3xl my-8'>The Shelf</h1>
         <Link to='/books/create'>
           <MdOutlineAddBox className = 'text-sky-800 text-4xl'/>
         </Link>
