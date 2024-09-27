5b7f4799f7a9efc57a1a25a16922980da88b193c
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-26 22:31:45 -0700

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,14 @@ const app = express();
 app.use(express.json());
 
 // middleware for handling CORS
-app.use(cors());
+app.use(
+    cors({
+        origin: 'https://fullstackedshelf.netlify.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
-// app.use(
-//     cors({
-//         origin: 'http://localhost:3000',
-//         methods: ['GET', 'POST','PUT', 'DELETE'],
-//         allowedHeaders: ['Content-Type'],
-//     })
-// );
 
 app.get('/', (request, response) => {
     console.log(request)
