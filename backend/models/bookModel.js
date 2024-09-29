438240a6ff5e54850889a305ebbf4c56626eaf67
nate-nguyen-22 <nnguy149@ucsc.edu>
2024-09-28 21:34:26 -0700

    number

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -12,7 +12,7 @@ const bookSchema = mongoose.Schema(
             required:true,
         },
         publishYear: {
-            type: String,
+            type: Number,
             required:true,
         },
         comment: {
